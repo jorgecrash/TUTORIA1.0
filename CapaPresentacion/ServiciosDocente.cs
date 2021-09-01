92233e7b888ff9dca27d0b07e1ca8bb63efad830
katheryne <OEM@LAPTOP-IUT020T4>
2021-08-31 22:41:23 -0500

    subiendo moq eliminar

--- CapaPresentacion/ServiciosDocente.cs
+++ CapaPresentacion/ServiciosDocente.cs
@@ -14,8 +14,6 @@ namespace CapaPresentacion
         public virtual bool eliminar_docente(string codigo)
         {
             int i;
-
-
             conexion.Open();
             SqlCommand cmd = new SqlCommand("SP_ELIMINARDOCENTE", conexion);
             cmd.CommandType = CommandType.StoredProcedure;
