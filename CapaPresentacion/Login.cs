cdbeb8037268e282cbf1c1759933cc787271b938
angel espinal huaman <angel_placido19992@hotmail.com>
2021-09-05 19:12:11 -0500

    PRUEBA

--- CapaPresentacion/Login.cs
+++ CapaPresentacion/Login.cs
@@ -28,7 +28,7 @@ namespace CapaPresentacion
             try
             {
 
-                using (SqlConnection conexion = new SqlConnection("Server=.;Integrated Security=yes; Database=Tutorias"))
+                using (SqlConnection conexion = new SqlConnection("Server=HOLA;Integrated Security=yes; Database=Tutorias"))
                 {
                     conexion.Open();
                     //_usuario = txtusuario.Text;
